a9ad12a5168a738381b9bc9985bb275b7349dcec
sidd1990 <siddattri@gmail.com>
2018-05-17 15:55:17 -0500

    redux setup for application

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import combinedReducers from './reducers'
 
 const App = () => {
   return(
+    <Provider store={createStore(redcers)}>
     <View />
+    </Provider>
   );
 };
 
