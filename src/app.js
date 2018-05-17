77391fdc9906681f12acf4296b5cf9ae0866389a
sidd1990 <siddattri@gmail.com>
2018-05-17 13:59:47 -0500

    Project setup

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const App = () => {
+  return(
+    <View />
+  );
+};
+
+export default App;
